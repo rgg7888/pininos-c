8d58ef2befa421e0139b2159a61cea90d96ac727
rgg7888 <rgg7888@outlook.com>
2021-05-18 11:23:01 -0600

    separando textos

---
+++ Frase.c
@@ -0,0 +1,9 @@
+printf("\n");
+puts("-------------------------------------");
+puts("Frase atribuida a : ");
+puts("Dijkstra - A principios de los setenta");
+puts("-------------------------------------");
+puts("Las pruebas solo muestran la presencia de errores,");
+puts("no su ausencia. No se puede probar que un programa");
+puts("es correcto (exacto) solo se puede mostrar que es incorrecto");
+puts("-------------------------------------");
